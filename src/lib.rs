6ea145109c6709b7e024716225a5c8efd8115c77
cloud971 <chasam@pdx.edu>
2018-07-20 00:01:29 -0700

    bob finished

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+
+
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+
+    let message = message.trim();
+
+    if message.is_empty() || message.find(char::is_alphanumeric).is_none() {
+        "Fine. Be that way!"
+    }
+    else if message.ends_with("!") && message.find(char::is_lowercase).is_none() {
+        "Whoa, chill out!"
+    }
+    else if message.find(char::is_lowercase).is_none() && message.ends_with("?"){
+        "Calm down, I know what I'm doing!"
+    }
+    else if message.find(char::is_lowercase).is_none(){
+        "Whoa, chill out!"
+    }
+    else if message.ends_with("?"){
+        "Sure."
+    }
+    else{
+        "Whatever."
+    }
 }
